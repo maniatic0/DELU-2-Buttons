e501051d3586d5d64ff1c363b55b8226135cf232
Christian Oliveros <christianol_01@hotmail.com>
2018-08-02 23:28:26 -0400

    Escape Button

--- Assets/Scripts/QuitGame.cs
+++ Assets/Scripts/QuitGame.cs
@@ -7,7 +7,7 @@ public class QuitGame : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey("cancel"))
+        if (Input.GetButtonDown("Cancel"))
         {
             Application.Quit();
         }
